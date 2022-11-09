5079e12bccddf7a80c3149403a8938744097fcc8
ajaidanial <ajaidanial@gmail.com>
2022-11-09 16:49:32 +0530

    added

--- script/install.sh
+++ script/install.sh
@@ -8,10 +8,12 @@ python -m venv venv
 
 pip install -r requirements.txt
 
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
 
 echo "All done"
 
 # Windows firewall disable or add port
 # To Open port to the public world: Windows firewall in control panel add the port in advanced settings | restart comp
 # REF: https://learn.microsoft.com/en-us/answers/questions/291348/can39t-open-ports-in-windows-10.html
+
+# Make sure to run in 0.0.0.0:8000
