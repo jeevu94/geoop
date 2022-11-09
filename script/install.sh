2fed62534f6f573d2c7d11c8737e7f4fcb9cc85e
ajaidanial <ajaidanial@gmail.com>
2022-11-09 16:28:35 +0530

    added

--- script/install.sh
+++ script/install.sh
@@ -11,3 +11,7 @@ pip install -r requirements.txt
 python manage.py runserver
 
 echo "All done"
+
+# Windows firewall disable or add port
+# To Open port to the public world: Windows firewall in control panel add the port in advanced settings
+# REF: https://learn.microsoft.com/en-us/answers/questions/291348/can39t-open-ports-in-windows-10.html
