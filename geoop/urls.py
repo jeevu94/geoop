0b9861399d72453002b13b60fef1302da0526298
jeevu94 <jeevan.jeevu94@gmail.com>
2022-11-09 15:38:46 +0530

    landing page added

--- geoop/urls.py
+++ geoop/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', lambda request: HttpResponse("<h1>Hello World!</h1>"), name='home')
 ]
